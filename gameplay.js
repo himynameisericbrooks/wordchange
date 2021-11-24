5fc9485cb2b16263d72056dc4ab5489bff44a9db
Eric Brooks <ericbrookspro@gmail.com>
2021-11-24 01:09:17 -0600

    gameplay.js planned and started // gamplay.html developed accordingly

--- gameplay.js
+++ gameplay.js
@@ -1,13 +1,20 @@
-const startword = "lemon"
-const endword = "ladle"
+const startword = "lemon";
+var starthtml = document.querySelector("#start-word");
+starthtml.innerHTML = "Starting word: " + startword;
+
+var currentword = startword;
+var currenthtml = document.querySelector("#current-word");
+currenthtml.innerHTML = "Current word: " + currentword;
+
+const destinationword = "ladle";
+var destinationhtml = document.querySelector("#destination-word");
+destinationhtml.innerHTML = "Destination word: " + destinationword;
+
 const validwords = ["a", "as", "at", "ab", "ad", "ah", "al", "am", "an", "ap", "aw", "ax", "ba", "bc", "be", "bi", "bo", "bp", "br", "bs", "by", "ca", "cc", "cd", ]
 
 console.log("It is your turn:")
 
 console.log("bye")
-
-// generate a random number
-var secretNum = Math.floor((Math.random() * 100) + 1);
 
 
 // Attempts counter
@@ -27,39 +34,38 @@ button.addEventListener("click", playGame, false);
 
 function playGame()
 {
-    guess = parseInt(input.value);
+    guess = input.value;
 
 // if they guess wrong, tell them if they were too high or too low and have them guess again
-        if (guess < secretNum) {
-    output.innerHTML = '<span>That was attempt number ' + tryCount + '.' + '<br></span> You guessed too low. <br>Try again. <br><span>I believe in you! :)</span';
+    output.innerHTML = '<span>' + 'Number of changes so far: ' + tryCount + '<br>' + ' You changed the word to: ' + input.value + " Now it's someone else's turn turn." + '</span>';
     tryCount +=1;
-    }
-
-    else if (guess > secretNum) {
-    output.innerHTML = '<span>That was attempt number ' + tryCount + '.' + '<br></span> You guessed too high. <br>Try again. <br><span>I think you might get it this time. Or maybe not :)</span>';
-    tryCount += 1;
-    }
-
-// when the user guesses correctly, tell them that they guessed correctly and tell them the number of tries it took them
-
-    else if (guess == secretNum) {
-    output.innerHTML = "That's it! " + secretNum + " was the secret number! <br>You guessed correctly! <span>It only took you " + tryCount + " tries to figure it out.</span>";
-    }
 
     // If a non-number is submitted
-    else {
-        output.innerHTML = 'That is not a valid number. <br> <span>Try again!</span>';
-        }
+//     else {
+//         output.innerHTML = 'That is not a valid number. <br> <span>Try again!</span>';
+//         }
 }
 
-// animate the button
-var classes = ["light", "", "heavy", "extraheavy"];
-var i = 1;
-$("#toggleweight").click(function () {
-  $(".btn").removeClass(classes[i]);
-  i++;
-  if (i >= classes.length) {
-    i = 0;
-  }
-  $(".btn").addClass(classes[i]);
-});
+
+
+// Display starting word and the destination word
+
+
+
+// Display current word
+
+
+
+// Tell the player what is happening in the game:
+// If it is your turn, this will tell you that it is your turn and will explain that you should change one letter in the current word to make it more similar to the destination word
+// If it is not your turn, this will let you know the user name of the active player
+
+
+
+// When it is your turn, an input button and text input field will display
+// The input field will have the input text of the current word by default, but the button will only except the input if it is a legal change
+
+
+
+// If the button is pushed with an illegal change, it will tell the user that that word is not a legal change and ask for a different word
+// If the button is pushed with a legal word, the html output will let you know that it was a legal play and above instances of current word will be replaced by your input. It will no longer be your turn

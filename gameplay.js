b6de024d5cac750622bb93fa8cb50c01dc703460
Eric Brooks <ericbrookspro@gmail.com>
2021-11-29 23:57:00 -0600

    troubleshooting gameplay.js require and hookup issue

--- gameplay.js
+++ gameplay.js
@@ -1,29 +1,29 @@
-const awords = require('./valid-words/awords')
-const bwords = require('./valid-words/bwords')
-const cwords = require('./valid-words/cwords')
-const dwords = require('./valid-words/dwords')
-const ewords = require('./valid-words/ewords')
-const fwords = require('./valid-words/fwords')
-const gwords = require('./valid-words/gwords')
-const hwords = require('./valid-words/hwords')
-const iwords = require('./valid-words/iwords')
-const jwords = require('./valid-words/jwords')
-const kwords = require('./valid-words/kwords')
-const lwords = require('./valid-words/lwords')
-const mwords = require('./valid-words/mwords')
-const nwords = require('./valid-words/nwords')
-const owords = require('./valid-words/owords')
-const pwords = require('./valid-words/pwords')
-const qwords = require('./valid-words/qwords')
-const rwords = require('./valid-words/rwords')
-const swords = require('./valid-words/swords')
-const twords = require('./valid-words/twords')
-const uwords = require('./valid-words/uwords')
-const vwords = require('./valid-words/vwords')
-const wwords = require('./valid-words/wwords')
-const xwords = require('./valid-words/xwords')
-const ywords = require('./valid-words/ywords')
-const zwords = require('./valid-words/zwords')
+// const awords = require('./valid-words/awords')
+// const bwords = require('./valid-words/bwords')
+// const cwords = require('./valid-words/cwords')
+// const dwords = require('./valid-words/dwords')
+// const ewords = require('./valid-words/ewords')
+// const fwords = require('./valid-words/fwords')
+// const gwords = require('./valid-words/gwords')
+// const hwords = require('./valid-words/hwords')
+// const iwords = require('./valid-words/iwords')
+// const jwords = require('./valid-words/jwords')
+// const kwords = require('./valid-words/kwords')
+// const lwords = require('./valid-words/lwords')
+// const mwords = require('./valid-words/mwords')
+// const nwords = require('./valid-words/nwords')
+// const owords = require('./valid-words/owords')
+// const pwords = require('./valid-words/pwords')
+// const qwords = require('./valid-words/qwords')
+// const rwords = require('./valid-words/rwords')
+// const swords = require('./valid-words/swords')
+// const twords = require('./valid-words/twords')
+// const uwords = require('./valid-words/uwords')
+// const vwords = require('./valid-words/vwords')
+// const wwords = require('./valid-words/wwords')
+// const xwords = require('./valid-words/xwords')
+// const ywords = require('./valid-words/ywords')
+const zwords = require('./valid-words/zwords.js');
 
 const startword = "lemon";
 var starthtml = document.querySelector("#start-word");
@@ -38,15 +38,15 @@ const destinationword = "ladle";
 var destinationhtml = document.querySelector("#destination-word");
 destinationhtml.innerHTML = "Destination word: " + destinationword;
 
-console.log("bye")
+console.log(zwords)
 
 
 // Attempts counter
-var count = 1;
+// var count = 1;
 
 // input and output
-var input = document.querySelector("#input");
-var output = document.querySelector("#output");
+// var input = document.querySelector("#input");
+// var output = document.querySelector("#output");
 
 // button stuff
 // var button = document.querySelector("button");
@@ -54,7 +54,7 @@ var output = document.querySelector("#output");
 // button.addEventListener("click", playGame, false);
 
 // function playGame()
-{
+// {
     // output.innerHTML = '<span>' + 'Number of changes so far: ' + count + '<br>' + ' You changed the word to: ' + input.value + " Now it's someone else's turn turn." + '</span>';
     // tryCount +=1;
 // }
@@ -78,4 +78,4 @@ var output = document.querySelector("#output");
 
 
 // If the button is pushed with an illegal change, it will tell the user that that word is not a legal change and ask for a different word
-// If the button is pushed with a legal word, the html output will let you know that it was a legal play and above instances of current word will be replaced by your input. It will no longer be your turn
+// If the button is pushed with a legal word, the html output will let you know that it was a legal play and above instances of current word will be replaced by your input. It will no longer be your tur
